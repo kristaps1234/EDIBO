fa941fcea87a4b93a103f2f79e464ffbbe345249
kristaps1234 <67696837+kristaps1234@users.noreply.github.com>
2020-07-21 21:07:07 +0300

    Update app.module.ts

--- AngularTest/HelloWorld/src/app/app.module.ts
+++ AngularTest/HelloWorld/src/app/app.module.ts
@@ -1,27 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ProfileService} from './profile.service';
-import { AppRoutingModule } from './app-routing.module';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+import { NavigationComponent } from './navigation/navigation.component';
+
 import { AppComponent } from './app.component';
-import{ DataService} from './data.service';
-import { CurrencyModule } from './currency/currency.module'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-
-
+import { CurrencyModule } from './currency/currency.module';
+import { WeatherModule } from './weather/weather.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatSidenavModule,
     CurrencyModule,
+    WeatherModule,
   ],
-  providers: [
-    ProfileService,
-    DataService,
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
