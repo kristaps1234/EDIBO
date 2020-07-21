4cc14244e48adb439412fad4342fcaf7bf8e8ca5
kristaps1234 <67696837+kristaps1234@users.noreply.github.com>
2020-07-21 21:03:50 +0300

    Update app-routing.module.ts

--- AngularTest/HelloWorld/src/app/app-routing.module.ts
+++ AngularTest/HelloWorld/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: '', component: AppComponent, pathMatch: 'full' },
+  { path: 'navigationTest', component: NavigationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule]  
 })
 export class AppRoutingModule { }
